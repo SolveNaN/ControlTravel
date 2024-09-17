13fc9930e09890f475b7adcd02823df1e5489858
SolveNaN <dagoberto6993@gmail.com>
2024-09-16 20:48:10 -0500

    first commit

---
+++ ControlTravelApp/Connection.cs
@@ -0,0 +1,8 @@
+﻿namespace ControWellApp
+{
+    public class Connection
+    {
+        public string server = "https://localhost:7186";
+        public string blobserverfiles = "https://generalfilesblazorapp.blob.core.windows.net/pemmas";
+    }
+}
---
+++ ControlTravelApp/Program.cs
@@ -0,0 +1,32 @@
+using ControlTravelApp.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddHttpClient();//incluye http client para peticiones api
+//incluye autorizacion
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
