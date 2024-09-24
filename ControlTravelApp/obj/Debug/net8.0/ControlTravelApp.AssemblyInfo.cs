e1899759981932e095d366a409da74dd687d3687
Huesly <hueslyc@gmail.com>
2024-09-23 21:34:06 -0500

    Cambios de textos

--- ControlTravelApp/obj/Debug/net8.0/ControlTravelApp.AssemblyInfo.cs
+++ ControlTravelApp/obj/Debug/net8.0/ControlTravelApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlTravelApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e627300a29ddc058ad6895e82a4474318fce41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2df84baaf2f13d92a106505325e0c63ec48e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlTravelApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlTravelApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
